140f860dc071ed61c08a798466f922f30347159b
Kim Minh Thang <thangved02@gmail.com>
2023-05-16 00:30:37 +0700

    Init project

---
+++ main.js
@@ -0,0 +1,13 @@
+import "./style.css";
+
+fetch("https://api.github.com/users/thangved/repos?per_page=100")
+  .then((res) => res.json())
+  .then((repos) => {
+    for (const repo of repos) {
+      document.getElementById("gh-repos").innerHTML += `<li class="my-2">
+        <a href="${repo.html_url}" class="text-blue-700  hover:underline">[${
+        repo.language || "Mixed"
+      }] ${repo.description || repo.html_url}</a>
+      </li>`;
+    }
+  });
