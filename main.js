4a2eab921703f10693bcdb31ce2c695588c15e66
Kim Minh Thang <thangved02@gmail.com>
2023-05-16 00:43:16 +0700

    Setup deploy

--- main.js
+++ main.js
@@ -5,7 +5,9 @@ fetch("https://api.github.com/users/thangved/repos?per_page=100")
   .then((repos) => {
     for (const repo of repos) {
       document.getElementById("gh-repos").innerHTML += `<li class="my-2">
-        <a href="${repo.html_url}" class="text-blue-700  hover:underline">[${
+        <a href="${
+          repo.html_url
+        }" target="_blank" class="text-blue-700  hover:underline">[${
         repo.language || "Mixed"
       }] ${repo.description || repo.html_url}</a>
       </li>`;
